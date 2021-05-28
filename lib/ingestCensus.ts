6862081e4b4925f383aa6292f668c19e86c621c3
Daniel Reynolds <danielreynolds101@gmail.com>
2021-05-28 14:35:14 -0600

    added exec promise

--- lib/ingestCensus.ts
+++ lib/ingestCensus.ts
@@ -1,10 +1,16 @@
 import { getFieldsAndValidateCSV } from './util';
+import assert = require('assert');
 
-export default function ingestCensus(filepath: string, indexes: string[]) {
+export default async function ingestCensus(filepath: string, indexes: string[]) {
     if (!indexes.length) {
         indexes = [ 'GISJOIN' ];
     }
     console.log(`Ingesting ${filepath} as a census table. Indexing on [${indexes}]`);
 
+    const fieldsInCSV = await getFieldsAndValidateCSV(filepath);
+    for (const index of indexes) {
+        assert(fieldsInCSV.includes(index), "Specified index is not in dataset!!!")
+    }
+    
 
 }
--- lib/util.ts
+++ lib/util.ts
@@ -1,21 +1,40 @@
-import csv = require('csv-parser')
-import fs = require('fs')
+import csv = require('csv-parser');
+import fs = require('fs');
+import { exec } from 'child_process';
 
 export const getFieldsAndValidateCSV = (path: string) => {
     try {
         return new Promise<string[]>(resolve => {
             let fields = new Set<string>();
+            let returnFields: string[] = [];
             fs.createReadStream(path)
                 .pipe(csv())
-                .on('data', (data) => {fields = new Set<string>([...fields, ...Object.keys(data)])})
+                .on('data', (data) => { fields = new Set<string>([ ...fields, ...Object.keys(data) ]) })
                 .on('end', () => {
-                    console.log(`Fields of ${path} are ${fields}`);
+                    returnFields = [ ...fields ]
+                    console.log(`Fields of ${path} are ${returnFields}`);
+                    resolve(returnFields);
                 });
-            resolve([...fields]);
         })
     }
     catch (e) {
         console.error(e)
         console.error(`Bad CSV @${path}`);
+        throw new Error("Bad CSV!");
     }
+}
+
+export const execCommand = async (cmd: string) => {
+    return new Promise<boolean>(resolve => {
+        exec(cmd, (err, stdout, stderr) => {
+            if (err) {
+                throw new Error(`Bad cli of ${cmd}`)
+            }
+
+            // the *entire* stdout and stderr (buffered)
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+            resolve(true);
+        });
+    })
 }
