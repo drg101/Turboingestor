7757c9625dee8d675a10037faddaaf41ee669fc1
Daniel Reynolds <danielreynolds101@gmail.com>
2021-05-28 12:23:58 -0600

    changes

--- lib/mongoBindings.ts
+++ lib/mongoBindings.ts
@@ -1,4 +1,14 @@
 import mongodb = require('mongodb');
 import assert = require('assert');
 const { MongoClient }  = mongodb;
+import { dbname, dbport } from './constants';
+const url = `mongodb://localhost:${dbport}`;
+
+
+MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log('Connected successfully to server');
+    const db = client.db(dbname);
+  
+  });
 
