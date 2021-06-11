d1bd2eec14519fe7db8246630950915dd8283f34
Daniel Reynolds <danielreynolds101@gmail.com>
2021-06-11 16:17:30 -0600

    part 1 of csv

--- lib/ingestNeon.ts
+++ lib/ingestNeon.ts
@@ -1,6 +1,7 @@
 import fs = require('fs');
 import { randomString } from './util'
 import csv = require('csv-parser');
+import readline = require('readline');
 
 const ingestNeon = async (name: string, filepath: string) => {
     let files = fs.readdirSync(filepath);
@@ -23,7 +24,7 @@ const ingestNeon = async (name: string, filepath: string) => {
 
     const timeSeriesFileName = `${name}_time_series_${uniqueRun}.csv`
     console.log(`Creating Time Series: ${timeSeriesFileName}`)
-    fs.openSync(`./out/${timeSeriesFileName}`, 'w');
+    fs.appendFileSync(`./out/${timeSeriesFileName}`, 'SITE,epoch_time');
 
     const labelMapFileName = `${name}_label_map_${uniqueRun}.json`
     console.log(`Creating Label map: ${labelMapFileName}`)
@@ -33,6 +34,7 @@ const ingestNeon = async (name: string, filepath: string) => {
     console.log(`Creating Locations: ${locationsFileName}`)
     fs.appendFileSync(`./out/${locationsFileName}`, 'SITE,PRODUCT,LAT,LONG\n');
 
+    let timeSeriesHeaderIsDone = false;
     for (const siteCode of siteCodes) {
         let relevantPackages = [];
         for (const release of releases) {
@@ -49,6 +51,8 @@ const ingestNeon = async (name: string, filepath: string) => {
             const { domainCode, month } = relevantPackage;
             const folder = findFileMatch(new RegExp(`^NEON\.${domainCode}\.${siteCode}\.${productCode}\.${month}\..*$`));
             console.log(`Package folder is ${folder}`)
+
+
             if (!hasLocationForSiteCode) {
                 console.log(`Getting sensor pos for ${siteCode}`)
                 for (const file of relevantPackage.files) {
@@ -59,14 +63,63 @@ const ingestNeon = async (name: string, filepath: string) => {
                                 .pipe(csv())
                                 .on('data', (data) => { posStream.destroy(); resolve(data); })
                         });
-                        fs.appendFileSync(`./out/${locationsFileName}`, `${siteCode},${productCode},${Number(referenceLatitude)},${Number(referenceLongitude)}\n`);
+                        fs.appendFileSync(`./out/${locationsFileName}`, `${siteCode},${name},${Number(referenceLatitude)},${Number(referenceLongitude)}\n`);
                         break;
                     }
                 }
                 hasLocationForSiteCode = true;
             }
+
+            const tsRegex = /^.*BP_30min.*$/g
+            let tsFile: string = "";
+            for (const file of relevantPackage.files) {
+                if (file.fileName.match(tsRegex)) {
+                    tsFile = file.fileName;
+                    console.log(`Time series file is ${tsFile}`)
+                    break;
+                }
+            }
+
+            if (!timeSeriesHeaderIsDone) {
+                console.log(`Adding time series header to out!`)
+                const fileStream = fs.createReadStream(`${filepath}/${folder}/${tsFile}`);
+                const rl = readline.createInterface({
+                    input: fileStream,
+                    crlfDelay: Infinity
+                });
+                for await (const line of rl) {
+                    fs.appendFileSync(`./out/${timeSeriesFileName}`, `${line}\n`);
+                    break;
+                }
+                timeSeriesHeaderIsDone = true;
+            }
+
+            console.log(`Collection epoch times from ${folder}`)
+            let times: number[] = [];
+            await new Promise<void>(resolve => {
+                fs.createReadStream(`${filepath}/${folder}/${tsFile}`)
+                .pipe(csv())
+                .on('data', (data) => { times.push(new Date(data.startDateTime).valueOf()) })
+                .on('end', () => { resolve() })
+            });
+
+            console.log(`Pasting time series for ${folder}`)
+            const fileStream = fs.createReadStream(`${filepath}/${folder}/${tsFile}`);
+            const rl = readline.createInterface({
+                input: fileStream,
+                crlfDelay: Infinity
+            });
+            let index = 0;
+            for await (const line of rl) {
+                if(index !== 0){
+                    fs.appendFileSync(`./out/${timeSeriesFileName}`, `${siteCode},${times.shift()},${line}\n`);
+                }
+                index++;
+            }
+
             break;
         }
+        break;
     }
 }
 
