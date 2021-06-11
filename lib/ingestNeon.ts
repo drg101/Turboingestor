4e959d1b516fe81fac7a74dec47ace95d5f41bd8
Daniel Reynolds <danielreynolds101@gmail.com>
2021-06-11 12:59:37 -0600

    more progress

--- lib/ingestNeon.ts
+++ lib/ingestNeon.ts
@@ -1,9 +1,21 @@
 import fs = require('fs');
 import { randomString } from './util'
+import csv = require('csv-parser');
 
 const ingestNeon = async (name: string, filepath: string) => {
-    console.log(`Neon manifest is ${filepath}`)
-    const manifest = JSON.parse(fs.readFileSync(filepath, 'utf8'));
+    let files = fs.readdirSync(filepath);
+
+    const findFileMatch = (regexp: RegExp) => {
+        for (const file of files) {
+            if (file.match(regexp)) {
+                return file;
+            }
+        }
+    }
+
+    const manifestFp = findFileMatch(/^.*\.manifest\..*\.json$/g);
+    console.log(`Neon manifest is ${manifestFp}`)
+    const manifest = JSON.parse(fs.readFileSync(`${filepath}/${manifestFp}`, 'utf8'));
     const { siteCodes, productCode, productName, releases } = manifest;
     console.log(`Sitecodes are: ${siteCodes}`)
     const uniqueRun = randomString(4);
@@ -21,16 +33,41 @@ const ingestNeon = async (name: string, filepath: string) => {
     console.log(`Creating Locations: ${locationsFileName}`)
     fs.openSync(`./out/${locationsFileName}`, 'w');
 
-    for(const siteCode of siteCodes){
+    for (const siteCode of siteCodes) {
         let relevantPackages = [];
-        for(const release of releases) {
-            for(const releasePackage of release.packages){
-                if(releasePackage.siteCode === siteCode){
+        for (const release of releases) {
+            for (const releasePackage of release.packages) {
+                if (releasePackage.siteCode === siteCode) {
                     relevantPackages.push(releasePackage);
                 }
             }
         }
-        console.log(relevantPackages)
+        relevantPackages = relevantPackages.sort((a, b) => new Date(a.month).valueOf() - new Date(b.month).valueOf());
+
+        let hasLocationForSiteCode = false;
+        for (const relevantPackage of relevantPackages) {
+            const { domainCode, month } = relevantPackage;
+            const folder = findFileMatch(new RegExp(`^NEON\.${domainCode}\.${siteCode}\.${productCode}\.${month}\..*$`));
+            console.log(`Package folder is ${folder}`)
+            if (!hasLocationForSiteCode) {
+                console.log(`Getting sensor pos for ${siteCode}`)
+                for (const file of relevantPackage.files) {
+                    if (file.fileName.match(/^.*sensor_positions.*.csv$/g)) {
+                        console.log(`File with positions is: ${file.fileName}`)
+                        const posData = await new Promise(resolve => {
+                            const posStream = fs.createReadStream(`${filepath}/${folder}/${file.fileName}`)
+                                .pipe(csv())
+                                .on('data', (data) => { posStream.destroy(); resolve(posStream); })
+                        });
+                        console.log(posData)
+                        break;
+                    }
+                }
+                hasLocationForSiteCode = true;
+            }
+            //console.log(relevantPackages)
+            break;
+        }
         break;
     }
 }
