fa9ea5df17fe490755369af244dda37867cb5c2f
Daniel Reynolds <danielreynolds101@gmail.com>
2021-06-17 11:02:25 -0600

    things

--- download.ts
+++ download.ts
@@ -3,6 +3,7 @@ import { getJSON, randomString, execCommand } from './lib/util';
 import chalk = require('chalk');
 import yargs = require('yargs/yargs');
 import { neonToken } from './lib/apiTokens'
+import readline = require("readline");
 
 type dictArr = { [ key: string ]: string[] };
 
@@ -14,7 +15,7 @@ interface resCode {
 };
 type dict = { [ key: string ]: string };
 
-const WAIT_TIME = 500;
+const WAIT_TIME = 600;
 let queueLen = 1;
 const waitForTurn = async () => {
     return new Promise<void>(resolve => {
@@ -30,22 +31,59 @@ const waitForTurn = async () => {
 const downloadAndUnzip = async ({month, siteCode, dataProductTitle, dataProductCode, url, out}: dict) => {
     console.log(`Downloading ${month} for ${siteCode}, ${dataProductTitle}`)
     const zipfile = `${out}/tmp/${siteCode}_${dataProductCode}_${month}.zip`
+    console.log(url)
     await waitForTurn();
-    await execCommand(`wget -O ${zipfile} ${url}?apiToken=${neonToken}`);
+    console.log(chalk.bgRed(url))
+    await execCommand(`wget -O ${zipfile} ${url}`);
     const outDir = `${out}/${dataProductCode}/${siteCode}_${month}`;
     fs.mkdirSync(outDir);
     await execCommand(`unzip ${zipfile} -d ${outDir}`);
 }
 
-const downloadNeon = async (out: string) => {
+const getConsoleInput = async (q: string) => {
+    return new Promise<string>(resolve => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.question(q, function(a) {
+            rl.close();
+            resolve(a);
+        });
+    });
+}
+
+const clearOld = async (out: string) => {
     await execCommand(`rm -rf ${out}/*`);
     fs.mkdirSync(`${out}/tmp`);
+}
+
+const downloadNeon = async (out: string) => {
+    await clearOld(out);
     const rid = randomString(4);
     let resCodes: resCode = {};
     await waitForTurn();
-    const sites = (await getJSON(`https://data.neonscience.org/api/v0/sites?apiToken=${neonToken}`)).data;
+    const sites = (await getJSON(`https://data.neonscience.org/api/v0/sites`)).data;
+
+    let products = new Set<string>();
+    for (const { siteCode, dataProducts } of sites) {
+        for (const { dataProductTitle } of dataProducts) {
+            products.add(dataProductTitle);
+        }
+    }
+
+    let productsArr: string[] = [...products];
+
+    fs.writeFileSync(`out/productsInput.json`, JSON.stringify(productsArr, null, 4));
+    await getConsoleInput("Done editing?");
+    productsArr = JSON.parse(fs.readFileSync(`out/productsInput.json`).toString());
+    console.log(productsArr)
+
     for (const { siteCode, dataProducts } of sites) {
         for (const { dataProductTitle, dataProductCode, availableMonths, availableDataUrls } of dataProducts) {
+            if(!productsArr.includes(dataProductTitle)){
+                continue;
+            }
             if (!fs.existsSync(`${out}/${dataProductCode}`)) {
                 fs.mkdirSync(`${out}/${dataProductCode}`);
             }
@@ -87,7 +125,6 @@ const downloadNeon = async (out: string) => {
 
             fs.writeFileSync(`${out}/codes_${rid}.json`, JSON.stringify(resCodes, null, 4));
         }
-        break;
     }
 
     fs.writeFileSync(`${out}/codes_${rid}.json`, JSON.stringify(resCodes, null, 4));
@@ -98,5 +135,6 @@ const downloadNeon = async (out: string) => {
         out: { type: 'string', requiresArg: true, demandOption: true },
     }).argv;
 
-    downloadNeon(out);
+    await downloadNeon(out);
+    console.log("done")
 })()
